8278997758af98f1c5fa006648c8e0a0ba5e7a04
PawelBoczkowski <78612365+PawelBoczkowski@users.noreply.github.com>
2021-11-18 21:01:27 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,80 @@
+from asyncio.windows_events import NULL
+import discord
+from discord import channel
+from discord.message import MessageType
+import os.path
+from email_sender import EmailSender
+import re
+
+
+emails = list()
+client = discord.Client()
+
+
+#if os.path.isfile('emails.txt'):
+    #with open('emails.txt') as file:
+        #for line in file:
+            #if line not in emails:
+                #emails.append(line)
+
+
+def write_emails(mails, *args):
+    for arg in args:
+        mails.remove(arg)
+    for mail in mails:
+        with open('emails.txt','a') as f:
+            f.write(mail+"\n")
+
+def send_email(): 
+    gmail_user = 'playnityrecruitment@gmail.com'
+    gmail_pwd = 'Playnity123'
+
+    try:
+        es = EmailSender('smtp.gmail.com',465)
+        es.login(gmail_user, gmail_pwd)
+        for email in emails:
+            #es.send("test","PlayNity Scholarship Invitation",email)
+            print("successfully sent the mail to "+email)
+            emails.remove(email)
+    except: print("failed to send mail to "+email)
+
+def refresh_file(file):
+    file.close()
+    open(file, "a")
+
+@client.event
+async def on_ready():
+    print('Logged in as:'+client.user.name)
+
+
+@client.event
+async def on_message(message):
+    if message.content == "!list":
+        for email in emails:
+            await client.send_message(message.channel, content=str(email))
+
+    elif message.content[:4] == "!add":
+        email = message.content[5:]
+        print("Trying to add "+message.content[5:]+" to emails")
+        if email not in emails:
+            print("Added "+message.content[5:]+" to emails")
+            emails.append(message.content[5:])
+            #print("Dodaje emaila append")
+            write_emails(emails)
+
+        else:
+            print("Email is already in emails")
+
+    elif message.content[:4] == "!del":
+        print("Removed "+message.content[5:]+" from emails")
+        write_emails(emails, message.content[5:])
+        
+
+    elif message.type is MessageType.default:
+        if str(message.author) == 'pawel#0407':
+            for email in emails:
+                print(f"email wyslany do {email}")
+                #send_email()
+
+
+client.run('OTAwNzQ5NjUwNTkwNTkzMDM1.YXF2ew.IvhTnY3Zf70737iZYVhoOtaNPU8')
